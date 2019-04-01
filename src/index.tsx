53b769f479f7553ee90e9e465281a98d9043b260
Oscar Eckbo <oscar.eckbo@nav.no>
2019-04-01 15:00:13 +0200

    [FO-2098] legger til tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import App from './App';
 import env from './app/utils/environment';
-import NAVSPA from "./NAVSPA";
+import NAVSPA from './NAVSPA';
 
 // if (env.isDevelopment) {
 //     require('./mock');
 // }
 
 NAVSPA.eksporter('veilarbvedtaksstottefs', App);
-
-
