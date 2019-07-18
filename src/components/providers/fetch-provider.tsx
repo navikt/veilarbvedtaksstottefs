76d6ab73e6a922178e87000cd928c58d288ea725
Alexander Gård <alexander.gard@nav.no>
2019-07-18 10:23:10 +0200

    FO-2366 La til predefinerte kilder for brukere som har nynorsk som målform

--- src/components/providers/fetch-provider.tsx
+++ src/components/providers/fetch-provider.tsx
@@ -4,17 +4,20 @@ import { VedtakData } from '../../utils/types/vedtak';
 import UnderOppfolging from '../../utils/types/under-oppfolging';
 import { FetchState, initialFetchState } from '../../utils/fetch-utils';
 import { Features } from '../../api/feature-toggle-api';
+import { MalformData } from '../../api/person-api';
 
 export interface AppFetchState {
     vedtak: FetchState<VedtakData[]>;
     underOppfolging: FetchState<UnderOppfolging>;
     features: FetchState<Features>;
+    malform: FetchState<MalformData>
 }
 
 const initialState: AppFetchState = {
     vedtak: initialFetchState,
     underOppfolging: initialFetchState,
     features: initialFetchState,
+    malform: initialFetchState
 };
 
 export const { GlobalStateProvider: FetchStateProvider, useGlobalState: useFetchState } = createGlobalState(initialState);
--- src/components/skjema/opplysninger/vis-opplysning/vis-opplysning.tsx
+++ src/components/skjema/opplysninger/vis-opplysning/vis-opplysning.tsx
@@ -3,18 +3,21 @@ import { Checkbox } from 'nav-frontend-skjema';
 import { Opplysning } from '../opplysninger';
 import { erDefaultOpplysning } from '../../skjema-utils';
 import './vis-opplysning.less';
+import { useFetchState } from '../../../providers/fetch-provider';
 
-interface VisopplysningProps {
+interface VisOpplysningProps {
     opplysning: Opplysning;
     handleOpplysning: () => void;
     onChange: (opplysning: Opplysning) => void;
     erSistEndretIndeks: boolean;
 }
 
-export function VisOpplysning(props: VisopplysningProps) {
+export function VisOpplysning(props: VisOpplysningProps) {
+    const [malformData] = useFetchState('malform');
     const checked = Object.values(props.opplysning)[0];
     const tekst = Object.keys(props.opplysning)[0];
-    const kanRedigeres = !erDefaultOpplysning(tekst);
+    const malform = malformData.data ? malformData.data.malform : null;
+    const kanRedigeres = !erDefaultOpplysning(tekst, malform);
 
     return (
         <div className="vis-opplysning">
