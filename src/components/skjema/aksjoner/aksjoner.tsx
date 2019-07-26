c5dbede7da970e6d6736e64d16f6e704fa4728a5
Alexander Gård <alexander.gard@nav.no>
2019-07-26 15:32:06 +0200

    KAIZEN-0 La til spinner på aksjoner i skjema

--- src/components/skjema/aksjoner/aksjoner.tsx
+++ src/components/skjema/aksjoner/aksjoner.tsx
@@ -1,23 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './aksjoner.less';
 import { Flatknapp, Hovedknapp, Knapp } from 'nav-frontend-knapper';
 import { ReactComponent as SlettIkon } from './slett.svg';
 import { ModalType, useModalStore } from '../../../stores/modal-store';
+import { SkjemaData } from '../../../pages/vedtakskjema/vedtakskjema-side';
+import { fetchWithInfo } from '../../../rest/utils';
+import { lagOppdaterVedtakUtkastFetchInfo } from '../../../rest/api';
+import { useViewStore, ViewType } from '../../../stores/view-store';
+import { useAppStore } from '../../../stores/app-store';
+import { useFetchStore } from '../../../stores/fetch-store';
+import { useSkjemaStore } from '../../../stores/skjema-store';
 
 interface AksjonerProps {
-    handleForhandsvis: (e: any) => void;
-    handleLagreOgTilbake: (e: any) => void;
+    vedtakskjema: SkjemaData;
+    harForsoktForhandsvisning: () => void;
 }
 
 function Aksjoner(props: AksjonerProps) {
-    const {showModal} = useModalStore();
+    const { fnr } = useAppStore();
+    const { vedtak } = useFetchStore();
+    const { changeView } = useViewStore();
+    const { showModal } = useModalStore();
+    const { validerSkjema } = useSkjemaStore();
+
+    const [visForhandsvisngLaster, setVisForhandsvisngLaster] = useState(false);
+    const [visLagreVedtakLaster, setVisLagreVedtakLaster] = useState(false);
+
+    function sendDataTilBackend() {
+        return fetchWithInfo(lagOppdaterVedtakUtkastFetchInfo({ fnr, skjema: props.vedtakskjema }))
+            .catch(() => {
+                setVisForhandsvisngLaster(false);
+                setVisLagreVedtakLaster(false);
+                showModal(ModalType.FEIL_VED_LAGRING);
+            });
+    }
+
+    function handleForhandsvis() {
+
+        if (!validerSkjema()) {
+            props.harForsoktForhandsvisning();
+            return;
+        }
+
+        setVisForhandsvisngLaster(true);
+
+        sendDataTilBackend()
+            .then(() => {
+                changeView(ViewType.FORHANDSVISNING);
+            });
+    }
+
+    function handleLagreOgTilbake() {
+
+        setVisLagreVedtakLaster(true);
+
+        sendDataTilBackend()
+            .then(() => {
+                vedtak.fetch({ fnr });
+                changeView(ViewType.HOVEDSIDE);
+                showModal(ModalType.VEDTAK_LAGRET_SUKSESS);
+            });
+    }
+
     return (
         <div className="aksjoner">
             <div className="aksjoner__knapper">
-                <Hovedknapp mini={true} htmlType="submit" onClick={props.handleForhandsvis}>
+                <Hovedknapp
+                    spinner={visForhandsvisngLaster}
+                    disabled={visForhandsvisngLaster}
+                    mini={true}
+                    htmlType="submit"
+                    onClick={handleForhandsvis}
+                >
                     Forhåndsvis og ferdigstill
                 </Hovedknapp>
-                <Knapp mini={true} htmlType="button" onClick={props.handleLagreOgTilbake}>
+                <Knapp
+                    spinner={visLagreVedtakLaster}
+                    disabled={visLagreVedtakLaster}
+                    mini={true}
+                    htmlType="button"
+                    onClick={handleLagreOgTilbake}
+                >
                     Lagre og gå tilbake
                 </Knapp>
             </div>
