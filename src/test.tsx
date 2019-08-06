100709f26856da5d9b92f9c849487dbc7b8c5d50
Alexander Gård <alexander.gard@nav.no>
2019-08-06 13:58:30 +0200

    KAIZEN-0 La til husky

---
+++ src/test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ViewController } from './components/view-controller';
+import { DataFetcher } from './components/datafetcher';
+import { PrelanseringSjekk } from './components/sjekk/prelansering-sjekk';
+import { NasjonalTilgangSjekk } from './components/sjekk/nasjonal-tilgang-sjekk';
+import StoreProvider from './stores/store-provider';
+import { ModalController } from './components/modal-controller';
+import './app.less';
+
+interface AppProps {
+    fnr: string;
+    enhet: string;
+}
+
+function App(props: AppProps) {
+    return (
+        <main className="veilarbvedtaksstottefs">
+            <StoreProvider fnr={props.fnr} enhetId={props.enhet}>
+                <NasjonalTilgangSjekk fnr={props.fnr}>
+                    <DataFetcher fnr={props.fnr}>
+                        <PrelanseringSjekk>
+                            <ViewController/>
+                            <ModalController/>
+                        </PrelanseringSjekk>
+                    </DataFetcher>
+                </NasjonalTilgangSjekk>
+            </StoreProvider>
+        </main>
+    );
+}
+
+export default App;
