fac53d4d62c110938dc7163ba351421a468d6a12
Alexander Gård <alexander.gard@nav.no>
2019-08-27 11:54:58 +0200

    KAIZEN-0 Flyttet prelansering sjekk høyere opp

--- src/components/sjekk/prelansering-sjekk.tsx
+++ src/components/sjekk/prelansering-sjekk.tsx
@@ -1,9 +1,30 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect } from 'react';
 import { Prelansering } from '../../pages/prelansering/prelansering';
 import { PRELANSERING_TOGGLE } from '../../rest/data/features';
 import { useFetchStore } from '../../stores/fetch-store';
+import { hasAnyFailed, isAnyNotStartedOrPending, isNotStarted } from '../../rest/utils';
+import Spinner from '../spinner/spinner';
+import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 
 export function PrelanseringSjekk(props: PropsWithChildren<any>) {
 	const { features } = useFetchStore();
+
+	useEffect(() => {
+		if (isNotStarted(features)) {
+			features.fetch(null);
+		}
+	}, [features]);
+
+	if (isAnyNotStartedOrPending([features])) {
+		return <Spinner />;
+	} else if (hasAnyFailed([features])) {
+		return (
+			<AlertStripeFeil className="vedtaksstotte-alert">
+				Det oppnås for tiden ikke kontakt med alle baksystemer.
+				Vi jobber med å løse saken. Vennligst prøv igjen senere.
+			</AlertStripeFeil>
+		);
+	}
+
 	return features.data[PRELANSERING_TOGGLE] ? <Prelansering /> : props.children;
 }
