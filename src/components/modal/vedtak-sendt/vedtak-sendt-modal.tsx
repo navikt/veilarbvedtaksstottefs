096095b485cbcb9597a7d30b6e61bc87dbfdd843
Sneha Desai <sneha.desai@nav.no>
2020-05-18 08:35:37 +0200

    FO-2908: Endre melding på vedtak sendt modal

--- src/components/modal/vedtak-sendt/vedtak-sendt-modal.tsx
+++ src/components/modal/vedtak-sendt/vedtak-sendt-modal.tsx
@@ -6,31 +6,19 @@ import { ModalProps } from '../modal-props';
 import { useModalStore } from '../../../stores/modal-store';
 import './vedtak-sendt-modal.less';
 
-interface VedtakSendtModalProps extends ModalProps{
-	sendesVedtakDigitalt?: boolean;
-}
+export function VedtakSendtModal(props: ModalProps) {
+    const { hideModal } = useModalStore();
 
-export function VedtakSendtModal(props: VedtakSendtModalProps) {
-	const { hideModal } = useModalStore();
-
-	const varselIkon = props.sendesVedtakDigitalt
-		? VarselIkonType.VEDTAK_SENDT_DIGITALT
-		: VarselIkonType.VEDTAK_SENDT_BREV;
-
-	const tekst = props.sendesVedtakDigitalt
-		? 'Vedtaket blir sendt til brukers digitale postkasse'
-		: 'Vedtaket blir sendt til bruker via post';
-
-	return (
-		<VarselModal
-			isOpen={props.isOpen}
-			contentLabel="Vedtaket sendt til bruker"
-			onRequestClose={hideModal}
-			varselIkonType={varselIkon}
-			portalClassName="vedtak-sendt-modal"
-		>
-			<Systemtittel className="vedtak-sendt-modal__tekst">{tekst}</Systemtittel>
-			<Hovedknapp onClick={hideModal}>OK</Hovedknapp>
-		</VarselModal>
-	);
+    return (
+            <VarselModal
+                isOpen={props.isOpen}
+                contentLabel="Vedtaket sendt til bruker"
+                onRequestClose={hideModal}
+                varselIkonType={VarselIkonType.VEDTAK_SENDT}
+                portalClassName="vedtak-sendt-modal"
+            >
+                <Systemtittel className="vedtak-sendt-modal__tekst">Oppfølgingsvedtaket er sendt til bruker</Systemtittel>
+                <Hovedknapp mini={true} htmlType="button" onClick={hideModal}>OK</Hovedknapp>
+            </VarselModal>
+        );
 }
