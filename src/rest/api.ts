231fb9695db148bda63dd1c7ff3b049ad159d842
Alexander Gård <alex.gaard@hotmail.com>
2020-08-04 13:53:02 +0200

    KAIZEN-0 Spesifiser same-origin for at credentials skal bli med i eldre browsere

--- src/rest/api.ts
+++ src/rest/api.ts
@@ -33,6 +33,8 @@ export const VEILARBPERSON_API = '/veilarbperson/api';
 export const VEILARBVEDTAKSSTOTTE_API = '/veilarbvedtaksstotte/api';
 export const VEILARBVEILEDER_API = '/veilarbveileder/api';
 
+const credentials = 'same-origin';
+
 export const HEADERS_WITH_JSON_CONTENT = {
 	'Content-Type': 'application/json'
 };
@@ -43,22 +45,27 @@ export const lagHentFeaturesFetchInfo = (): FetchInfo => {
 };
 
 export const lagHentOppfolgingDataFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBOPPFOLGING_API}/oppfolging?fnr=${params.fnr}`
 });
 
 export const lagHentTilgangTilKontorFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBOPPFOLGING_API}/oppfolging/veilederTilgang?fnr=${params.fnr}`
 });
 
 export const lagHentMalformFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBPERSON_API}/person/${params.fnr}/malform`
 });
 
 export const lagHentVeilederFetchInfo = (): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEILEDER_API}/veileder/me`
 });
 
 export const lagNyttUtkastFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/utkast`,
 	method: 'POST',
 	headers: HEADERS_WITH_JSON_CONTENT,
@@ -68,6 +75,7 @@ export const lagNyttUtkastFetchInfo = (params: FnrFetchParams): FetchInfo => ({
 export const lagOppdaterVedtakUtkastFetchInfo = (params: OppdaterUtkastFetchParams): FetchInfo => {
 	params.skjema.opplysninger = mapOpplysningerFraBokmalTilBrukersMalform(params.skjema.opplysninger, params.malform);
 	return {
+		credentials,
 		url: `${VEILARBVEDTAKSSTOTTE_API}/utkast/${params.vedtakId}`,
 		method: 'PUT',
 		headers: HEADERS_WITH_JSON_CONTENT,
@@ -76,23 +84,28 @@ export const lagOppdaterVedtakUtkastFetchInfo = (params: OppdaterUtkastFetchPara
 };
 
 export const lagHentUtkastFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/utkast?fnr=${params.fnr}`
 });
 
 export const lagHentFattedeVedtakFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/vedtak/fattet?fnr=${params.fnr}`
 });
 
 export const lagHentArenaVedtakFetchInfo = (params: FnrFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/vedtak/arena?fnr=${params.fnr}`
 });
 
 export const lagFattVedtakFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/utkast/${params.vedtakId}/fattVedtak`,
 	method: 'POST',
 });
 
 export const lagSendDialogFetchInfo = (params: SendDialogFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/meldinger?vedtakId=${params.vedtakId}`,
 	method: 'POST',
 	headers: HEADERS_WITH_JSON_CONTENT,
@@ -100,25 +113,30 @@ export const lagSendDialogFetchInfo = (params: SendDialogFetchParams): FetchInfo
 });
 
 export const lagHentMeldingerFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/meldinger?vedtakId=${params.vedtakId}`
 });
 
 export const lagSlettUtkastFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/utkast/${params.vedtakId}`,
 	method: 'DELETE'
 });
 
 export const lagErUtkastGodkjentFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/utkast/${params.vedtakId}/erGodkjent`,
 	method: 'GET'
 });
 
 export const lagTaOverUtkastFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/utkast/${params.vedtakId}/overta`,
 	method: 'POST'
 });
 
 export const lagHentOyblikksbildeFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/vedtak/${params.vedtakId}/oyeblikksbilde`
 });
 
@@ -131,21 +149,25 @@ export const lagHentArenaVedtakPdfUrl = (dokumentInfoId: string, journalpostId: 
 	`${VEILARBVEDTAKSSTOTTE_API}/vedtak/arena/pdf?dokumentInfoId=${dokumentInfoId}&journalpostId=${journalpostId}`;
 
 export const lagStartBeslutterProsessFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/beslutter/start?vedtakId=${params.vedtakId}`,
 	method: 'POST'
 });
 
 export const lagBliBeslutterFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/beslutter/bliBeslutter?vedtakId=${params.vedtakId}`,
 	method: 'POST'
 });
 
 export const lagGodkjennVedtakFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/beslutter/godkjenn?vedtakId=${params.vedtakId}`,
 	method: 'POST'
 });
 
 export const lagOppdaterBeslutterProsessStatusFetchInfo = (params: VedtakIdFetchParams): FetchInfo => ({
+	credentials,
 	url: `${VEILARBVEDTAKSSTOTTE_API}/beslutter/status?vedtakId=${params.vedtakId}`,
 	method: 'PUT'
 });
