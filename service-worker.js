868fc97a6f8109d8bff4718b2fda60b6e119fc8a
matiasvinjevoll <matiasvinjevoll@users.noreply.github.com>
2020-08-17 07:49:53 +0000

    Automated deployment: Mon Aug 17 07:49:53 UTC 2020 7b66f1d3d980e0c40f13e5ac1ee0127a5119b7de

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/veilarbvedtaksstottefs/precache-manifest.4a5c816660e9a787a006697ebf244d5f.js"
+  "/veilarbvedtaksstottefs/precache-manifest.42c5e5cadb5422510f129fc1c01fec7b.js"
 );
 
 self.addEventListener('message', (event) => {
