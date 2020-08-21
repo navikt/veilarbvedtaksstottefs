e6a1691abcd33b565783b0c2e9a370380211e16e
Alexander Gård <alex.gaard@hotmail.com>
2020-08-21 15:16:41 +0200

    FO-2994 Tilpassing av brevvisning lokalt og i demo

--- src/mock/konstanter.ts
+++ src/mock/konstanter.ts
@@ -1,6 +1,3 @@
 export const fnr = '00123456789';
 export const enhetId = '0118';
 export const enhetNavn = 'NAV Testheim';
-
-export const vedtaksBrevUrlLokal = '/testbrev.pdf';
-export const vedtaksbrevUrlGhPages = '/veilarbvedtaksstottefs/testbrev.pdf';
