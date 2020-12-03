c9833f0fc791b230db3a6e4961aa71f50fadc839
Alexander Gård <alex.gaard@hotmail.com>
2020-12-03 17:00:15 +0100

    La til oppsett for å automatisere UU tester

---
+++ cypress/integration/accessibility_spec.js
@@ -0,0 +1,27 @@
+function terminalLog(violations) {
+    cy.task(
+        'log',
+        `${violations.length} accessibility violation${
+            violations.length === 1 ? '' : 's'
+        } ${violations.length === 1 ? 'was' : 'were'} detected`
+    )
+    // pluck specific keys to keep the table readable
+    const violationData = violations.map(
+        ({ id, impact, description, nodes }) => ({
+            id,
+            impact,
+            description,
+            nodes: nodes.length
+        })
+    )
+
+    cy.task('table', violationData)
+}
+
+describe('Accessibility', () => {
+    it('Skal oppfylle krav til UU på hovedside', () => {
+        cy.visit('http://localhost:3666')
+        cy.injectAxe()
+        cy.checkA11y(null, null, terminalLog)
+    })
+})
