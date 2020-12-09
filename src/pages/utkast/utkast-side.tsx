ac10887c50d125db34eb648cdd6a7e136cbba8ca
Alexander Gård <alex.gaard@hotmail.com>
2020-12-09 10:49:00 +0100

    Bruk .app klasse fra nav-frontend for å sette default font styling

--- src/pages/utkast/utkast-side.tsx
+++ src/pages/utkast/utkast-side.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { EndreUtkastSkjema } from './endre-utkast-skjema';
 import './utkast-side.less';
-import { useSkjemaStore } from '../../stores/skjema-store';
-import { DialogPanelInnhold } from './dialog-panel/dialog-panel-innhold';
-import { DialogPanelHeader } from './dialog-panel/dialog-panel-header';
 import { UtkastFooter } from './footer/utkast-footer';
 import { DialogPanel } from './dialog-panel/dialog-panel';
 
@@ -12,20 +9,6 @@ export function UtkastSide() {
 	// return erAnsvarligVeileder
 	// 	? <EndreUtkastSkjema />
 	// 	: <LesUtkastSkjema />;
-	const {
-		opplysninger,
-		hovedmal,
-		innsatsgruppe,
-		begrunnelse,
-		sistOppdatert,
-		setSistOppdatert,
-		validerSkjema,
-		validerBegrunnelseLengde,
-		lagringStatus,
-		setLagringStatus
-	} = useSkjemaStore();
-
-	const vedtakskjema = { opplysninger, begrunnelse, innsatsgruppe, hovedmal };
 
 	return (
 		<div className="utkast-side-grid">
