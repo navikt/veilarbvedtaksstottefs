dff55dd532d1cff5f5ddc7c47f1305c8550de19f
Alexander Gård <alex.gaard@hotmail.com>
2020-12-15 15:36:48 +0100

    La til mock handler for frontendlogger

--- src/mock/api/index.ts
+++ src/mock/api/index.ts
@@ -4,9 +4,11 @@ import { veilarbvedtaksstotteHandlers } from './veilarbvedtaksstotte';
 import { veilarbpersonHandlers } from './veilarbperson';
 import { veilarbpersonflatefsHandlers } from './veilarbpersonflatefs';
 import { veilarbveilederHandlers } from './veilarbveileder';
+import { frontendloggerHandlers } from './frontendlogger';
 
 export const allHandlers: RequestHandlersList = [
 	...veilarbvedtaksstotteHandlers,
+	...frontendloggerHandlers,
 	...veilarboppfolgingHandlers,
 	...veilarbpersonHandlers,
 	...veilarbpersonflatefsHandlers,
