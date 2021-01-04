3498a86dd7a6d69529c7cff776294b1bd8fad8f9
Alexander Gård <alex.gaard@hotmail.com>
2021-01-04 15:37:33 +0100

    Fikset footer margin for skjermer større enn 1920px

--- src/page/utkast/footer/utkast-footer.tsx
+++ src/page/utkast/footer/utkast-footer.tsx
@@ -9,23 +9,37 @@ function calculateScrollBarWidth(): number {
 	return document.body.offsetWidth - document.body.clientWidth;
 }
 
+function checkIsMaxWidthOrLess(): boolean {
+	return document.body.clientWidth <= 1920;
+}
+
 export function UtkastFooter() {
 	const { kilder, hovedmal, innsatsgruppe, begrunnelse } = useSkjemaStore();
 
+	const [isMaxWidthOrLess, setIsMaxWidthOrLess] = useState(checkIsMaxWidthOrLess);
 	const [scrollBarWidth, setScrollBarWidth] = useState(calculateScrollBarWidth());
 
 	const vedtakskjema = { opplysninger: kilder, begrunnelse, innsatsgruppe, hovedmal };
 
+	const footerStyle = isMaxWidthOrLess ? { marginRight: `${scrollBarWidth}px` } : undefined;
+
 	useEffect(() => {
-		// Dette vil ikke fungere lokalt siden vi bruker "height: 100%" på body, men det vil fungere i test/prod
-		const handleEvent = () => setScrollBarWidth(calculateScrollBarWidth());
-		window.addEventListener('scroll', handleEvent);
-		return () => window.removeEventListener('scroll', handleEvent);
+		// Scroll vil ikke fungere lokalt siden vi bruker "height: 100%" på body, men det vil fungere i test/prod
+		const handleScrollEvent = () => setScrollBarWidth(calculateScrollBarWidth());
+		const handleResizeEvent = () => setIsMaxWidthOrLess(checkIsMaxWidthOrLess());
+
+		window.addEventListener('scroll', handleScrollEvent);
+		window.addEventListener('resize', handleResizeEvent);
+
+		return () => {
+			window.removeEventListener('scroll', handleScrollEvent);
+			window.removeEventListener('resize', handleResizeEvent);
+		};
 	}, []);
 
 	return (
 		<Footer className="utkast-footer">
-			<div style={{ marginRight: `${scrollBarWidth}px` }} className="utkast-footer__innhold">
+			<div style={footerStyle} className="utkast-footer__innhold">
 				<UtkastInnhold vedtakskjema={vedtakskjema} />
 				<DialogInnhold vedtakskjema={vedtakskjema} />
 			</div>
