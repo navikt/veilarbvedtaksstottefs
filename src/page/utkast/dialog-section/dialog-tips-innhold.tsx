9b2c820541dd8c870e4880c78637e5f005ea9964
Alexander Gård <alex.gaard@hotmail.com>
2021-01-08 13:30:03 +0100

    La til forklaring om innsyn i dialog i tips innhold

--- src/page/utkast/dialog-section/dialog-tips-innhold.tsx
+++ src/page/utkast/dialog-section/dialog-tips-innhold.tsx
@@ -5,10 +5,15 @@ export const DialogTipsInnhold = () => {
 	return (
 		<>
 			<Undertittel className="blokk-xs">Kollegaveiledning</Undertittel>
-			<Normaltekst>
+			<Normaltekst className="blokk-xxs">
 				Når du vurderer at bruker har liten mulighet til å jobbe eller bare kan jobbe delvis, skal vurderingen
 				din kvalitetssikres. Hensikten er at du får tilbakemeldinger på skjønnsvurderingene du har gjort og at
 				konklusjonen blir riktig.
+			</Normaltekst>
+
+			<Normaltekst>
+				Dialogen kan gjøres tilgjengelig i behandling av klager og innsynsbegjæringer. Når vedtaket er fattet
+				vil ikke dialogen være synlig i løsningen.
 			</Normaltekst>
 		</>
 	);
