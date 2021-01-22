f5cf051be90a7ca61bca1fc73aed6aea9f9881fd
Alexander Gård <alex.gaard@hotmail.com>
2021-01-22 10:39:58 +0100

    La inn lenke til beslutteroversikten i dialog header

--- src/page/utkast/dialog-section/dialog-section-header.tsx
+++ src/page/utkast/dialog-section/dialog-section-header.tsx
@@ -8,28 +8,57 @@ import { TipsPopover } from '../../../component/tips-popover/tips-popover';
 import Show from '../../../component/show';
 import { Label } from '../../../component/label/label';
 import { DialogToggleBtn } from '../dialog-section-minified/dialog-toggle-btn';
+import Lenke from 'nav-frontend-lenker';
+import tilOversiktBilde from './til-oversikt.svg';
+import { useTilgangStore } from '../../../store/tilgang-store';
+import env from '../../../util/environment';
 
 interface DialogPanelHeaderProps {
 	beslutterNavn?: OrNothing<string>;
 }
 
+function utledOversiktUrl(): string {
+	if (env.isRunningOnGhPages || env.isDevelopment) {
+		return 'https://navikt.github.io/beslutteroversikt';
+	} else if (env.erITestMiljo) {
+		return 'https://app-q1.adeo.no/beslutteroversikt';
+	} else {
+		return 'https://app.adeo.no/beslutteroversikt';
+	}
+}
+
 export function DialogSectionHeader(props: DialogPanelHeaderProps) {
+	const { erBeslutter } = useTilgangStore();
+
 	return (
 		<header className="dialog-section-header">
 			<div className="dialog-section-header__toggle-btn-wrapper">
 				<DialogToggleBtn />
 			</div>
-			<Undertittel className="dialog-section-header__tittel">Kollegaveiledning</Undertittel>
-			<TipsPopover
-				id="dialog-tips"
-				className="dialog-section-header__tips"
-				tipsInnhold={<DialogTipsInnhold />}
-				ariaLabel="Tips for kollegaveiledning"
-				orientering={PopoverOrientering.UnderHoyre}
-			/>
-			<Show if={props.beslutterNavn}>
-				<Label titleText="Ansvarlig" valueText={props.beslutterNavn} />
-			</Show>
+
+			<div className="dialog-section-header__innhold">
+				<Undertittel className="dialog-section-header__tittel">Kollegaveiledning</Undertittel>
+				<TipsPopover
+					id="dialog-tips"
+					className="dialog-section-header__tips"
+					tipsInnhold={<DialogTipsInnhold />}
+					ariaLabel="Tips for kollegaveiledning"
+					orientering={PopoverOrientering.UnderHoyre}
+				/>
+				<Show if={props.beslutterNavn}>
+					<Label titleText="Ansvarlig" valueText={props.beslutterNavn} />
+				</Show>
+
+				<Show if={erBeslutter}>
+					<Lenke
+						title="Til kvalitetssikring oversikt for 14a"
+						href={utledOversiktUrl()}
+						className="dialog-section-header__oversikt-lenke"
+					>
+						<img src={tilOversiktBilde} alt="Til kvalitetssikring oversikt for 14a" />
+					</Lenke>
+				</Show>
+			</div>
 		</header>
 	);
 }
--- src/util/environment.ts
+++ src/util/environment.ts
@@ -7,6 +7,12 @@ class Environment {
 		return process.env.REACT_APP_DEV === 'true';
 	}
 
+	get erITestMiljo() {
+		return (
+			window.location.hostname.split('.').findIndex(domain => ['app-q1', 'app-q0', 'dev'].includes(domain)) >= 0
+		);
+	}
+
 	get isRunningOnGhPages() {
 		return window.location.hostname === 'navikt.github.io';
 	}
