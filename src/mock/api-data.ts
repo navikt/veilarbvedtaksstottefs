7d423312e4a8815f08f0d5290ff17ae5a472c3a2
Alexander Gård <alex.gaard@hotmail.com>
2021-03-25 13:41:17 +0100

    Bruk endepunkt for gradvis utrulling istedenfor pilot toggle

--- src/mock/api-data.ts
+++ src/mock/api-data.ts
@@ -3,7 +3,6 @@ import TilgangTilBrukersKontor from '../util/type/tilgang-til-brukers-kontor';
 import { MalformData, MalformType } from '../api/veilarbperson';
 import {
 	FeatureToggles,
-	PILOT_TOGGLE,
 	PRELANSERING_INFO_OM_LOSNING_TOGGLE,
 	SKRU_AV_POLLING_DIALOG,
 	SKRU_AV_POLLING_UTKAST,
@@ -36,7 +35,6 @@ const malform: MalformData = {
 const features: FeatureToggles = {
 	[PRELANSERING_INFO_OM_LOSNING_TOGGLE]: true,
 	[STOPPE_VEDTAKSUTSENDING_TOGGLE]: false,
-	[PILOT_TOGGLE]: true,
 	[SKRU_AV_POLLING_UTKAST]: false,
 	[SKRU_AV_POLLING_DIALOG]: false
 };
