957b58451763eb77f3564d827c7408ec6bcfc1f6
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2022-02-10 14:52:45 +0100

    TK-110 Vis målform under begrunnelse når utkast vises i lesemodus

--- src/component/page/page.tsx
+++ src/component/page/page.tsx
@@ -11,7 +11,6 @@ interface PageProps {
 const Page = (props: PageProps) => {
 	return (
 		<div className={cls('page', props.className)}>
-			<h3>Test av branch deploy til Q1</h3>
 			<div className={cls('page__content', props.contentClassName)}>{props.children}</div>
 		</div>
 	);
