9e8f849b4c12b18c46b54859be15b7a5245af54e
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-06-17 10:32:26 +0200

    Legg til spraksjekk-pakken

---
+++ src/spraksjekk-intern/components/index.js
@@ -0,0 +1,12 @@
+// Analysis
+export { default as OrdTelling } from "./analysis/OrdTelling.tsx";
+export { default as LongWords } from "./analysis/LongWords.tsx";
+export { default as LongSentences } from "./analysis/LongSentences.tsx";
+export { default as LongParagraphs } from "./analysis/LongParagraphs.tsx";
+export { default as DublicateWords } from "./analysis/DublicateWords.tsx";
+export { default as GammelnavskCheck } from "./analysis/GammelnavskCheck.tsx";
+export { default as WordFrequency } from "./analysis/WordFrequency.tsx";
+export { default as Begrepskatalog } from "./analysis/Begrepskatalog.tsx";
+export { default as Nrkordliste } from "./analysis/Nrkordliste.tsx";
+export { default as Lix } from "./analysis/Lix.tsx";
+export { default as LixMelding } from "./analysis/LixMelding.tsx";
---
+++ src/spraksjekk-intern/data/index.js
@@ -0,0 +1,2 @@
+export { default as Gammelnavsk } from "./gammelnavsk.json";
+export { default as Nrkordliste } from "./nrkordliste.json";
