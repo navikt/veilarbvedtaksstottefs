72544185453a40b7790ae90f0da30e585a9ac9c6
JulieHillRoa <julie.hill.roa@nav.no>
2023-11-21 16:27:55 +0100

    Fikser noen mocklenker som ikke var oppdatert etter fjerning av fnr i api-kall

--- src/mock/api/veilarbpersonflatefs.ts
+++ src/mock/api/veilarbpersonflatefs.ts
@@ -2,7 +2,7 @@ import { RequestHandler, rest } from 'msw';
 import { hentFeatures } from '../api-data';
 
 export const veilarbpersonflatefsHandlers: RequestHandler[] = [
-	rest.get('/veilarbpersonflatefs/api/feature', (req, res, ctx) => {
+	rest.get('/obo-unleash/api/feature', (req, res, ctx) => {
 		return res(ctx.delay(500), ctx.json(hentFeatures()));
 	})
 ];
