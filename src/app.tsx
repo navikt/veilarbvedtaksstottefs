8fd9d4831c0590c9df411d675fd25cd9ad4f3577
Mathias S. Müller <mathias.sundt.muller@nav.no>
2024-02-02 16:45:48 +0100

    Importer ds-css som layer

--- src/app.tsx
+++ src/app.tsx
@@ -5,12 +5,12 @@ import { NasjonalTilgangSjekk } from './component/sjekk/nasjonal-tilgang-sjekk';
 import StoreProvider from './store/store-provider';
 import { ModalController } from './component/modal-controller';
 import { TabClickedListener } from './component/tab-clicked-listener';
-import './app.less';
 import Show from './component/show';
 import env from './util/environment';
 import { VarselController } from './component/varsel/varsel-controller';
 import { MockPanel } from './mock/component/mock-panel';
 import FeatureFetcher from './component/feature-fetcher';
+import './app.css';
 
 interface AppProps {
 	fnr: string;
