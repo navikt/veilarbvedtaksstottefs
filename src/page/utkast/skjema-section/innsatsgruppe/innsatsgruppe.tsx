4ff79cdfbd1993a34fb79bf946b9e7da4316528f
Mathias S. Müller <mathias.sundt.muller@nav.no>
2024-04-02 16:56:47 +0200

    Oppdater innsatsgruppe-radioknapper til nytt designsystem

--- src/page/utkast/skjema-section/innsatsgruppe/innsatsgruppe.tsx
+++ src/page/utkast/skjema-section/innsatsgruppe/innsatsgruppe.tsx
@@ -1,4 +1,3 @@
-import { Radio, RadioGruppe } from 'nav-frontend-skjema';
 import FeltHeader from '../felt-header/felt-header';
 import { InnsatsgruppeTipsInnhold } from './innsatsgruppe-tips-innhold';
 import { harSkrevetBegrunnelse, trengerKvalitetssikrer } from '../../../../util/skjema-utils';
@@ -15,8 +14,8 @@ import { useDataStore } from '../../../../store/data-store';
 import { erStandard, erVarigEllerGradertVarig, innsatsgruppeTekster } from '../../../../util/innsatsgruppe';
 import { useSkjemaStore } from '../../../../store/skjema-store';
 import { useDialogSection } from '../../../../store/dialog-section-store';
-import { Alert } from '@navikt/ds-react';
-import './innsatsgruppe.less';
+import { Alert, Radio, RadioGroup } from '@navikt/ds-react';
+import './innsatsgruppe.css';
 
 function Innsatsgruppe() {
 	const { innsatsgruppe, begrunnelse, setInnsatsgruppe, setHovedmal } = useSkjemaStore();
@@ -29,8 +28,8 @@ function Innsatsgruppe() {
 	return (
 		<div className="innsatsgruppe-felt" id="innsatsgruppe-scroll-to">
 			<FeltHeader
+				id="innsatsgruppe-tittel"
 				tittel="Innsatsgruppe"
-				tittelId="innsatsgruppe-tittel"
 				tipsId="innsatsgruppe-tips"
 				tipsInnhold={<InnsatsgruppeTipsInnhold />}
 				tipsAriaLabel="Tips for innsatsgruppe"
@@ -43,9 +42,9 @@ function Innsatsgruppe() {
 				</Alert>
 			)}
 			<InnsatsgruppeRadioButtons
-				handleInnsatsgruppeChanged={setInnsatsgruppe}
 				innsatsgruppe={innsatsgruppe}
-				setHovedmal={setHovedmal}
+				endreInnsatsgruppe={setInnsatsgruppe}
+				endreHovedmal={setHovedmal}
 			/>
 		</div>
 	);
@@ -54,9 +53,9 @@ function Innsatsgruppe() {
 export default Innsatsgruppe;
 
 interface InnsatsgruppeRadioProps {
-	handleInnsatsgruppeChanged: (e: InnsatsgruppeType) => void;
-	setHovedmal: (e: any) => void;
 	innsatsgruppe: OrNothing<InnsatsgruppeType>;
+	endreInnsatsgruppe: (e: InnsatsgruppeType) => void;
+	endreHovedmal: (e: any) => void;
 }
 
 function InnsatsgruppeRadioButtons(props: InnsatsgruppeRadioProps) {
@@ -72,7 +71,7 @@ function InnsatsgruppeRadioButtons(props: InnsatsgruppeRadioProps) {
 		) {
 			showModal(ModalType.BEKREFT_AVBRYT_BESLUTTER_PROSESS, { innsatsgruppe });
 		} else {
-			props.handleInnsatsgruppeChanged(innsatsgruppe);
+			props.endreInnsatsgruppe(innsatsgruppe);
 		}
 
 		if (trengerKvalitetssikrer(innsatsgruppe)) {
@@ -80,25 +79,24 @@ function InnsatsgruppeRadioButtons(props: InnsatsgruppeRadioProps) {
 		}
 
 		if (innsatsgruppe === InnsatsgruppeType.VARIG_TILPASSET_INNSATS) {
-			props.setHovedmal(null);
+			props.endreHovedmal(null);
 		}
 	}
 
 	return (
-		<div className="innsatsgruppe">
-			<RadioGruppe legend={null} feil={lagSkjemaelementFeilmelding(errors.innsatsgruppe)}>
-				{innsatsgruppeTekster.map(innsatsgruppeTekst => (
-					<Radio
-						key={innsatsgruppeTekst.value}
-						label={innsatsgruppeTekst.tittel}
-						name="innsatsgruppe"
-						value={innsatsgruppeTekst.value}
-						onKeyDown={swallowEnterKeyPress}
-						checked={props.innsatsgruppe === innsatsgruppeTekst.value}
-						onChange={(e: any) => handleInnsatsgruppeChanged(e.target.value)}
-					/>
-				))}
-			</RadioGruppe>
-		</div>
+		<RadioGroup
+			size="small"
+			legend="Valg av innsatsgruppe"
+			hideLegend
+			onChange={nyInnsatsgruppe => handleInnsatsgruppeChanged(nyInnsatsgruppe)}
+			value={props.innsatsgruppe}
+			error={lagSkjemaelementFeilmelding(errors.innsatsgruppe)}
+		>
+			{innsatsgruppeTekster.map(innsatsgruppetekst => (
+				<Radio key={innsatsgruppetekst.value} value={innsatsgruppetekst.value} onKeyDown={swallowEnterKeyPress}>
+					{innsatsgruppetekst.tittel}
+				</Radio>
+			))}
+		</RadioGroup>
 	);
 }
