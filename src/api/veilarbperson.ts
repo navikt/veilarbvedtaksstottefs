7860c14f778196cce8ec46c0264b0c00d4244772
JulieHillRoa <julie.hill.roa@nav.no>
2024-05-23 15:02:42 +0200

    LEgge ved behandlingsnummer på kall mot veilarbperson

--- src/api/veilarbperson.ts
+++ src/api/veilarbperson.ts
@@ -6,10 +6,11 @@ export enum MalformType {
 	nn = 'nn'
 }
 
+const behandlingsnummer = 'B642';
 export interface MalformData {
 	malform: MalformType | null;
 }
 
 export function fetchMalform(fnr: string): AxiosPromise<MalformData> {
-	return axiosInstance.post(`/veilarbperson/api/v3/person/hent-malform`, { fnr });
+	return axiosInstance.post(`/veilarbperson/api/v3/person/hent-malform`, { fnr, behandlingsnummer });
 }
