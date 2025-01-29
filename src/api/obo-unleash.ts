5c3d5d219394fb5634e96ce519ef65233ba908ae
Ingrid Fosså <ingrid.fossa@nav.no>
2025-01-29 13:12:58 +0100

    Legg til funksjonsbrytar frå Unleash for å vise vedtaksløysinga

--- src/api/obo-unleash.ts
+++ src/api/obo-unleash.ts
@@ -2,11 +2,13 @@ import { AxiosPromise } from 'axios';
 import { axiosInstance } from './utils';
 
 export const PRELANSERING_INFO_OM_LOSNING_TOGGLE = 'veilarbvedtaksstottefs.prelansering-info-om-losning';
+export const VIS_VEDTAKSLOSNING_14_A = 'veilarbvedtaksstotte.visVedtakslosning14a';
 
-export const ALL_TOGGLES = [PRELANSERING_INFO_OM_LOSNING_TOGGLE];
+export const ALL_TOGGLES = [PRELANSERING_INFO_OM_LOSNING_TOGGLE, VIS_VEDTAKSLOSNING_14_A];
 
 export interface FeatureToggles {
 	[PRELANSERING_INFO_OM_LOSNING_TOGGLE]: boolean;
+	[VIS_VEDTAKSLOSNING_14_A]: boolean;
 }
 
 export function fetchFeaturesToggles(): AxiosPromise<FeatureToggles> {
