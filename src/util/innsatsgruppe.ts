6986746162a02a93558bba34943436f6b81c5298
Ingrid Fosså <ingrid.fossa@nav.no>
2025-02-11 14:40:02 +0100

    Ta bort gamle innsatsgruppenamn fordi dei ikkje er i bruk meir

--- src/util/innsatsgruppe.ts
+++ src/util/innsatsgruppe.ts
@@ -18,34 +18,28 @@ export const erVarigEllerGradertVarig = (innsatsgruppe: OrNothing<InnsatsgruppeT
 
 export interface InnsatsgruppeTekst {
 	tittel: string;
-	undertekst: string;
 	value: InnsatsgruppeType;
 }
 
 export const innsatsgruppeTekster: InnsatsgruppeTekst[] = [
 	{
 		tittel: 'Gode muligheter',
-		undertekst: 'STANDARD INNSATS',
 		value: InnsatsgruppeType.STANDARD_INNSATS
 	},
 	{
 		tittel: 'Trenger veiledning',
-		undertekst: 'SITUASJONSBESTEMT INNSATS',
 		value: InnsatsgruppeType.SITUASJONSBESTEMT_INNSATS
 	},
 	{
 		tittel: 'Trenger veiledning, nedsatt arbeidsevne',
-		undertekst: 'SPESIELT TILPASSET INNSATS',
 		value: InnsatsgruppeType.SPESIELT_TILPASSET_INNSATS
 	},
 	{
 		tittel: 'Jobbe delvis',
-		undertekst: 'DELVIS, VARIG TILPASSET INNSATS',
 		value: InnsatsgruppeType.GRADERT_VARIG_TILPASSET_INNSATS
 	},
 	{
 		tittel: 'Liten mulighet til å jobbe',
-		undertekst: 'VARIG TILPASSET INNSATS',
 		value: InnsatsgruppeType.VARIG_TILPASSET_INNSATS
 	}
 ];
