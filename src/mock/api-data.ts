23de997e426ffa56811bcfb1e17f82a162bffb1c
Ingrid Fosså <ingrid.fossa@nav.no>
2025-03-18 10:00:38 +0100

    Legg til kommentar slik at ikkje folk skrur av toggle i demoløysinga

--- src/mock/api-data.ts
+++ src/mock/api-data.ts
@@ -3,7 +3,10 @@ import TilgangTilBrukersKontor from '../util/type/tilgang-til-brukers-kontor';
 import { MalformData, MalformType, Navn } from '../api/veilarbperson';
 import {
 	FeatureToggles,
-	HOVEDMAL_SKAFFE_ARBEID_UAVHENGIG_AV_ARBEIDSSOKERPERIODE,PRELANSERING_INFO_OM_LOSNING_TOGGLE, VIS_VEDTAKSLOSNING_14_A } from '../api/obo-unleash';
+	HOVEDMAL_SKAFFE_ARBEID_UAVHENGIG_AV_ARBEIDSSOKERPERIODE,
+	PRELANSERING_INFO_OM_LOSNING_TOGGLE,
+	VIS_VEDTAKSLOSNING_14_A
+} from '../api/obo-unleash';
 import { Veileder } from '../api/veilarbveileder';
 import { enhetId, enhetNavn, veileder1, veileder2, veileder3 } from './data';
 import { DialogMelding, SystemMelding } from '../api/veilarbvedtaksstotte/meldinger';
@@ -57,7 +60,7 @@ const arbeidssokerperiode = {
 const mockFeatures: FeatureToggles = {
 	[PRELANSERING_INFO_OM_LOSNING_TOGGLE]: true,
 	[HOVEDMAL_SKAFFE_ARBEID_UAVHENGIG_AV_ARBEIDSSOKERPERIODE]: false,
-	[VIS_VEDTAKSLOSNING_14_A]: true
+	[VIS_VEDTAKSLOSNING_14_A]: true // Denne er på slik at demo-løysinga skal vise vedtaksløysinga fram til lansering
 };
 
 let innloggetVeileder: Veileder = {
