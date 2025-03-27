2eeed64f11e14ab61e05ed7ab0f33023f2510fca
JulieHillRoa <julie.hill.roa@nav.no>
2025-03-27 13:21:29 +0100

    Fjerne featuretoggle som hindret valg om skaffe arbeid om du ikke var i arb.soekerregisteret

--- src/page/utkast/skjema-section/hovedmal/hovedmal.tsx
+++ src/page/utkast/skjema-section/hovedmal/hovedmal.tsx
@@ -1,34 +1,14 @@
-import { useEffect, useState } from 'react';
-import { Alert, Radio, RadioGroup } from '@navikt/ds-react';
-import { ArbeidssokerPeriode } from '@navikt/arbeidssokerregisteret-utils';
+import { Radio, RadioGroup } from '@navikt/ds-react';
 import FeltHeader from '../felt-header/felt-header';
 import { hovedmalTekst } from '../../../../util/hovedmal';
 import { useSkjemaStore } from '../../../../store/skjema-store';
-import { useAppStore } from '../../../../store/app-store';
 import { HovedmalType, InnsatsgruppeType } from '../../../../api/veilarbvedtaksstotte';
 import { swallowEnterKeyPress } from '../../../../util';
-import { fetchAktivArbeidssokerperiode } from '../../../../api/veilarbperson';
-import { useDataStore } from '../../../../store/data-store';
-import { HOVEDMAL_SKAFFE_ARBEID_UAVHENGIG_AV_ARBEIDSSOKERPERIODE } from '../../../../api/obo-unleash';
 import './hovedmal.css';
 
 function Hovedmal() {
 	const { innsatsgruppe, hovedmal: valgtHovedmal, setHovedmal, errors } = useSkjemaStore();
 	const erVarigTilpassetInnsats = innsatsgruppe === InnsatsgruppeType.VARIG_TILPASSET_INNSATS;
-	const [arbeidssoekerperiode, setArbeidssoekerperiode] = useState<ArbeidssokerPeriode | null>(null);
-	const { fnr } = useAppStore();
-
-	const { features } = useDataStore();
-	const skaffeArbeidUavhengigAvArbeidssokerperiode =
-		features[HOVEDMAL_SKAFFE_ARBEID_UAVHENGIG_AV_ARBEIDSSOKERPERIODE];
-
-	const skaffeArbeidErIkkeValgbar = !skaffeArbeidUavhengigAvArbeidssokerperiode && !arbeidssoekerperiode;
-
-	useEffect(() => {
-		if (!arbeidssoekerperiode) {
-			fetchAktivArbeidssokerperiode(fnr).then(response => setArbeidssoekerperiode(response.data));
-		}
-	}, [arbeidssoekerperiode, fnr]);
 
 	return (
 		<div className="hovedmal-felt" id="hovedmal__scroll-til-feil">
@@ -47,20 +27,8 @@ function Hovedmal() {
 					</span>
 				) : (
 					<>
-						{skaffeArbeidErIkkeValgbar && (
-							<Alert size="small" variant="warning" inline>
-								Hovedmål <i>skaffe arbeid</i> kan ikke velges fordi personen ikke er registrert som
-								arbeidssøker.
-							</Alert>
-						)}
 						{Object.values(HovedmalType).map(hovedmaltype => (
-							<Radio
-								key={hovedmaltype}
-								value={hovedmaltype}
-								onKeyDown={swallowEnterKeyPress}
-								// Hvis brukeren ikke har en aktiv arbeidssøkerperiode (og feature-toggle er av), skal ikke hovedmål "Skaffe arbeid" kunne velges
-								disabled={skaffeArbeidErIkkeValgbar && hovedmaltype === HovedmalType.SKAFFE_ARBEID}
-							>
+							<Radio key={hovedmaltype} value={hovedmaltype} onKeyDown={swallowEnterKeyPress}>
 								{hovedmalTekst[hovedmaltype]}
 							</Radio>
 						))}
