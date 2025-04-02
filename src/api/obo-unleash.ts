0d3253b732e5894ae0d3cd9b1a54349e52a4a795
Ingrid Fosså <ingrid.fossa@nav.no>
2025-04-02 12:45:17 +0200

    Slett feature-toggle
    Co-authored-by: Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>

--- src/api/obo-unleash.ts
+++ src/api/obo-unleash.ts
@@ -2,13 +2,11 @@ import { AxiosPromise } from 'axios';
 import { axiosInstance } from './utils';
 
 export const PRELANSERING_INFO_OM_LOSNING_TOGGLE = 'veilarbvedtaksstottefs.prelansering-info-om-losning';
-export const VIS_VEDTAKSLOSNING_14_A = 'veilarbvedtaksstotte.visVedtakslosning14a';
 
-export const ALL_TOGGLES = [PRELANSERING_INFO_OM_LOSNING_TOGGLE, VIS_VEDTAKSLOSNING_14_A];
+export const ALL_TOGGLES = [PRELANSERING_INFO_OM_LOSNING_TOGGLE];
 
 export interface FeatureToggles {
 	[PRELANSERING_INFO_OM_LOSNING_TOGGLE]: boolean;
-	[VIS_VEDTAKSLOSNING_14_A]: boolean;
 }
 
 export function fetchFeaturesToggles(): AxiosPromise<FeatureToggles> {
