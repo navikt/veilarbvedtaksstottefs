c8e702e4978f0da76175b45fa1beeb439daf1f07
Kristina Skatun <36476370+kcs85@users.noreply.github.com>
2025-06-19 13:26:04 +0200

    Oppdater håndtering av ny 204 respons fra hent utkast  (#535)
    * Endre responskode for hent utkast til 204 ved manglende innhold
    
    * Bytt ut funksjon med sjekk av loading

--- src/component/data-fetcher.tsx
+++ src/component/data-fetcher.tsx
@@ -10,7 +10,7 @@ import { fetchOppfolging } from '../api/veilarboppfolging';
 import { fetchAktivArbeidssokerperiode, fetchMalform, fetchNavn } from '../api/veilarbperson';
 import { fetchUtkast } from '../api/veilarbvedtaksstotte/utkast';
 import { fetchInnloggetVeileder } from '../api/veilarbveileder';
-import { ifResponseHasData, hasAnyFailed, isAnyLoadingOrNotStarted } from '../api/utils';
+import { ifResponseHasData, hasAnyFailed, isAnyLoading } from '../api/utils';
 import { IkkeKontaktMedBaksystemFeilmelding } from './feilmelding/ikke-kontakt-med-baksystem-feilmelding';
 
 export function DataFetcher(props: { fnr: string; children: any }) {
@@ -73,7 +73,7 @@ export function DataFetcher(props: { fnr: string; children: any }) {
 	}, [innloggetVeilederFetcher, utkastFetcher]);
 
 	if (
-		isAnyLoadingOrNotStarted(
+		isAnyLoading(
 			fattedeVedtakFetcher,
 			oppfolgingFetcher,
 			malformFetcher,
@@ -91,9 +91,9 @@ export function DataFetcher(props: { fnr: string; children: any }) {
 			malformFetcher,
 			innloggetVeilederFetcher,
 			arenaVedtakFetcher,
-			navnFetcher
-		) ||
-		(utkastFetcher.error && utkastFetcher.error.response?.status !== 404) // API gir 404 dersom utkast ikke eksisterer
+			navnFetcher,
+			utkastFetcher
+		)
 	) {
 		return <IkkeKontaktMedBaksystemFeilmelding />;
 	}
