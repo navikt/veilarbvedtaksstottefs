448ad6fe59002cc21377c721d6a1345be638b32c
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2025-06-24 09:16:36 +0200

    Fiks skrivefeil

--- src/page/utkast/kan-ikke-distribueres-alert.tsx
+++ src/page/utkast/kan-ikke-distribueres-alert.tsx
@@ -4,7 +4,7 @@ export const KanIkkeDistribueresAlert = ({ kanDistribueres }: { kanDistribueres:
 	kanDistribueres ? null : (
 		<Alert variant="warning" id="kan-ikke-distribueres-alert">
 			Brukeren kan ikke varsles om vedtaket fordi vi ikke finner adresse eller telefonnummer. Forsøk å få brukeren
-			til å registrere kontakinformasjon før du sender ut vedtaket. <br /> Se{' '}
+			til å registrere kontaktinformasjon før du sender ut vedtaket. <br /> Se{' '}
 			<Link href="https://www.nav.no/kontaktinformasjon" target="_blank" rel="noopener noreferrer">
 				nav.no/kontaktinformasjon
 			</Link>
