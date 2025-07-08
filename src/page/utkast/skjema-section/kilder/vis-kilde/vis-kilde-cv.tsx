1ba43345ae7c067549188119cab9d63f97855e40
Kristina Skatun <kristinaskatun@gmail.com>
2025-07-08 15:01:51 +0200

    Fiks disabled på checkbox

--- src/page/utkast/skjema-section/kilder/vis-kilde/vis-kilde-cv.tsx
+++ src/page/utkast/skjema-section/kilder/vis-kilde/vis-kilde-cv.tsx
@@ -9,15 +9,18 @@ interface VisKildeProps {
 
 export function VisKildeCv({ kildenavn }: Readonly<VisKildeProps>) {
 	const { cvSomKilde } = useDataStore();
+	//eslint-disable-next-line no-console
+	console.log('cvSomKilde', cvSomKilde);
 
 	return (
 		<>
-			<Checkbox value={kildenavn} onKeyDown={swallowEnterKeyPress} disabled={cvSomKilde.cvKanBrukesSomKilde}>
+			<Checkbox value={kildenavn} onKeyDown={swallowEnterKeyPress} disabled={!cvSomKilde.cvKanBrukesSomKilde}>
 				{kildenavn}
 			</Checkbox>
 			{!cvSomKilde.cvKanBrukesSomKilde && (
 				<Alert size="small" variant="info" inline className="vis-kilde__alert">
-					Du kan ikke bruke denne kilden, fordi {cvSomKilde.begrunnelse ?? 'det ikke finnes et CV for denne brukeren.'}
+					Du kan ikke bruke denne kilden, fordi{' '}
+					{cvSomKilde.begrunnelse ?? 'det ikke finnes et CV for denne brukeren.'}
 				</Alert>
 			)}
 		</>
