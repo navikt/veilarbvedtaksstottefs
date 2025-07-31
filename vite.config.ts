b265379dbd806209215d1552178431255c38a3a2
Mathias S. Müller <mathias.sundt.muller@nav.no>
2025-07-31 11:20:24 +0200

    Konfigurer Vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest/config" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		manifest: 'asset-manifest.json',
+		sourcemap: true
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom'
+	}
+});
