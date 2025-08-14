c1729246c36da0b791d2120aaf597d007cda922c
Mathias S. Müller <mathias.sundt.muller@nav.no>
2025-08-14 14:23:57 +0200

    Bump masse, React til v18, TS til v5 og NavSPA til v6

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './app';
 import env from './util/environment';
 import NAVSPA from '@navikt/navspa';
@@ -9,21 +10,21 @@ import { enhetId, fnr } from './mock/data';
 dayjs.locale('nb');
 
 if (env.isDemo) {
-	import('./mock/index').then(({ worker }) => {
-		worker
-			.start({ serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js` } })
-			.then(() => {
-				// createRoot(document.getElementById('veilarbvedtaksstottefs-root') as HTMLElement).render();
-				ReactDOM.render(
-					<App fnr={fnr} enhet={enhetId} />,
-					document.getElementById('veilarbvedtaksstottefs-root')
-				);
-			})
-			.catch((e: Error) => {
-				// eslint-disable-next-line no-console
-				console.error('Unable to setup mocked API endpoints', e);
-			});
-	});
+	import('./mock/index')
+		.then(({ worker }) => {
+			worker
+				.start({ serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js` } })
+				.then(() => {
+					createRoot(document.getElementById('veilarbvedtaksstottefs-root') as HTMLElement).render(
+						<App fnr={fnr} enhet={enhetId} />
+					);
+				})
+				.catch((e: Error) => {
+					// eslint-disable-next-line no-console
+					console.error('Unable to setup mocked API endpoints', e);
+				});
+		}) // eslint-disable-next-line no-console
+		.catch(e => console.log('MSW - failed to import', e));
 } else {
 	NAVSPA.eksporter('veilarbvedtaksstottefs', App);
 }
