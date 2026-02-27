f5802960ae19abd6c5ca062ebc331f9f49d51145
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2026-02-27 11:11:41 +0100

    Skisse til videre diskusjon del 3

--- src/page/klagebehandling/formkrav-section/formkrav-section.tsx
+++ src/page/klagebehandling/formkrav-section/formkrav-section.tsx
@@ -1,4 +1,4 @@
-import { Radio, RadioGroup, Stack, VStack } from '@navikt/ds-react';
+import { Radio, RadioGroup, Stack, TextField, VStack } from '@navikt/ds-react';
 import { useEffect, useState } from 'react';
 
 interface FormkravSectionProps {
@@ -7,9 +7,11 @@ interface FormkravSectionProps {
 
 export function FormkravSection({ onChange }: FormkravSectionProps) {
 	const [klagefristOverholdt, setKlagefristOverholdt] = useState<boolean | undefined>();
+	const [klagefristOverstyres, setKlagefristOverstyres] = useState<boolean | undefined>();
 	const [klagerPartISaken, setKlagerPartISaken] = useState<boolean | undefined>();
 	const [klagePaaKonkreteElementer, setKlagePaaKonkreteElementer] = useState<boolean | undefined>();
 	const [erKlagenSignert, setErKlagenSignert] = useState<boolean | undefined>();
+	const [avvisningsAarsak, setAvvisningsAarsak] = useState<string | undefined>();
 
 	useEffect(() => {
 		const ferdig = [klagefristOverholdt, klagerPartISaken, klagePaaKonkreteElementer, erKlagenSignert].every(
@@ -26,13 +28,26 @@ export function FormkravSection({ onChange }: FormkravSectionProps) {
 					<Radio value={false}>Nei</Radio>
 				</Stack>
 			</RadioGroup>
+			{!klagefristOverholdt && (
+				<RadioGroup legend="Er unntak for klagefristen oppfylt?" size="small" onChange={setKlagefristOverstyres}>
+					<Stack gap="space-0 space-24" direction={{ xs: 'column', sm: 'row' }} wrap={false}>
+						<Radio value={true}>Ja, klager kan ikke lastes for å ha sendt inn klage etter fristen</Radio>
+						<Radio value={true}>Ja, av særlige grunner er det rimelig at klagen blir behandlet</Radio>
+						<Radio value={false}>Nei</Radio>
+					</Stack>
+				</RadioGroup>
+			)}
 			<RadioGroup legend="Er klager part i saken?" size="small" onChange={setKlagerPartISaken}>
 				<Stack gap="space-0 space-24" direction={{ xs: 'column', sm: 'row' }} wrap={false}>
 					<Radio value={true}>Ja</Radio>
 					<Radio value={false}>Nei</Radio>
 				</Stack>
 			</RadioGroup>
-			<RadioGroup legend="Klages det på konkrete elementer i vedtaket?" size="small" onChange={setKlagePaaKonkreteElementer}>
+			<RadioGroup
+				legend="Klages det på konkrete elementer i vedtaket?"
+				size="small"
+				onChange={setKlagePaaKonkreteElementer}
+			>
 				<Stack gap="space-0 space-24" direction={{ xs: 'column', sm: 'row' }} wrap={false}>
 					<Radio value={true}>Ja</Radio>
 					<Radio value={false}>Nei</Radio>
@@ -44,6 +59,11 @@ export function FormkravSection({ onChange }: FormkravSectionProps) {
 					<Radio value={false}>Nei</Radio>
 				</Stack>
 			</RadioGroup>
+			{(!(klagefristOverstyres || klagefristOverholdt) && klagerPartISaken && klagePaaKonkreteElementer && erKlagenSignert) && (
+				<TextField label="Begrunnelse for å avvise klage"
+						   value={avvisningsAarsak}
+						   onChange={e => setAvvisningsAarsak(e.target.value)} />
+			) }
 		</VStack>
 	);
 }
