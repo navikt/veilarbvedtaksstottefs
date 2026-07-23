6441c0b33a2703f91dbcaf8fb4437823178d2764
sigurdgroneng <sigurd.groneng@nav.no>
2026-07-23 12:19:08 +0200

    Bruk veilarboppfolging sitt graphql-api istedetfor hent-status

--- src/api/veilarboppfolging.ts
+++ src/api/veilarboppfolging.ts
@@ -8,8 +8,44 @@ export default interface OppfolgingData {
 	inaktivIArena: boolean;
 }
 
+interface OppfolgingStatusGraphQLData {
+	brukerStatus: {
+		krr?: { reservertIKrr: boolean } | null;
+		arena?: { inaktivIArena: boolean } | null;
+	} | null;
+	oppfolging?: { erUnderOppfolging?: boolean | null } | null;
+}
+
+const HENT_OPPFOLGING_STATUS_QUERY = `
+    query HentOppfolgingStatus($fnr: String!) {
+        brukerStatus(fnr: $fnr) {
+            krr {
+                reservertIKrr
+            }
+            arena {
+                inaktivIArena
+            }
+        }
+        oppfolging(fnr: $fnr) {
+            erUnderOppfolging
+        }
+    }
+`;
+
 export function fetchOppfolging(fnr: string): AxiosPromise<OppfolgingData> {
-	return axiosInstance.post(`/veilarboppfolging/api/v3/oppfolging/hent-status`, { fnr });
+	return axiosInstance
+		.post<{ data: OppfolgingStatusGraphQLData }>(`/veilarboppfolging/api/graphql`, {
+			query: HENT_OPPFOLGING_STATUS_QUERY,
+			variables: { fnr }
+		})
+		.then(res => ({
+			...res,
+			data: {
+				reservasjonKRR: res.data.data.brukerStatus?.krr?.reservertIKrr ?? false,
+				underOppfolging: res.data.data.oppfolging?.erUnderOppfolging ?? false,
+				inaktivIArena: res.data.data.brukerStatus?.arena?.inaktivIArena ?? false
+			}
+		}));
 }
 
 export function fetchTilgangTilBrukersKontor(fnr: string): AxiosPromise<TilgangTilBrukersKontor> {
